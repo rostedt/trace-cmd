10dd98df4bd75ec5629719aeb2e384adfd495fc6
Steven Rostedt <srostedt@redhat.com>
2009-10-13 00:57:39 -0400

    handle new lines between args
    If a new line was between args, it could fail the parsing.
    Be more flexible with newlines.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- parse-events.c
+++ parse-events.c
@@ -1195,7 +1195,7 @@ process_op(struct event *event, struct print_arg *arg, char **tok)
 
 		right = malloc_or_die(sizeof(*right));
 
-		type = read_token(&token);
+		type = read_token_item(&token);
 		*tok = token;
 
 		/* could just be a type pointer */
@@ -1683,12 +1683,18 @@ process_arg_token(struct event *event, struct print_arg *arg,
 
 int event_read_print_args(struct event *event, struct print_arg **list)
 {
-	enum event_type type;
+	enum event_type type = EVENT_ERROR;
 	struct print_arg *arg;
 	char *token;
 	int args = 0;
 
 	do {
+		if (type == EVENT_NEWLINE) {
+			free_token(token);
+			type = read_token_item(&token);
+			continue;
+		}
+
 		arg = malloc_or_die(sizeof(*arg));
 		memset(arg, 0, sizeof(*arg));
 
