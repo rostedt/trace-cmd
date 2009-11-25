9e47d25f3dacb1dcd9e0b18cb411026e8f7b338c
Steven Rostedt <srostedt@redhat.com>
2009-11-24 21:51:17 -0500

    Change test plugin to override hrtimer events start and expire_entry
    The hrtimer events hrtimer_start and hrtimer_expire_entry currently
    fail to parse. Override these events to show better data.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- test_plugin.c
+++ test_plugin.c
@@ -4,13 +4,28 @@
 
 #include "parse-events.h"
 
-static int handler(struct trace_seq *s, void *data, int size)
+static int timer_expire_handler(struct trace_seq *s, void *data, int size)
 {
+	void *hrtimer = data + 16;
+	long long now = *(long long *)(data + 24);
 	int ret;
 
-	ret = trace_seq_printf(s, "COMM: %s state is %d next is %s",
-			       (char *)(data+12), *(int *)(data + 40),
-			       (char *)(data+48));
+	ret = trace_seq_printf(s, "hrtimer=%p now=%llu",
+			       hrtimer, now);
+	return ret;
+}
+
+static int timer_start_handler(struct trace_seq *s, void *data, int size)
+{
+	void *hrtimer = data + 16;
+	void *function = data + 24;
+	long long expires = *(long long *)(data + 32);
+	long long soft = *(long long *)(data + 40);
+	int ret;
+
+	ret = trace_seq_printf(s, "hrtimer=%p function=%pf expires=%llu softexpires=%llu",
+			       hrtimer, function,
+			       expires, soft);
 	return ret;
 }
 
@@ -18,7 +33,11 @@ int PEVENT_PLUGIN_LOADER(void)
 {
 	printf("HELLO WORLD!!!\n");
 
-	pevent_register_event_handler(-1, "sched", "sched_switch",
-				      handler);
+	pevent_register_event_handler(-1, "timer", "hrtimer_expire_entry",
+				      timer_expire_handler);
+
+	pevent_register_event_handler(-1, "timer", "hrtimer_start",
+				      timer_start_handler);
+
 	return 0;
 }
