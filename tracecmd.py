ce91ba442d8d909a7a9fc554338a9576d23e9239
Johannes Berg <johannes@sipsolutions.net>
2010-05-20 10:07:53 +0200

    python: use pseudo-private fields
    This is more idiomatic python since most
    other code shouldn't access these fields.
    
    Acked-by: Darren Hart <dvhltc@us.ibm.com>
    Signed-off-by: Johannes Berg <johannes@sipsolutions.net>

--- tracecmd.py
+++ tracecmd.py
@@ -33,11 +33,11 @@ TODO: consider a complete class hierarchy of ftrace events...
 
 class Event(object):
     def __init__(self, pevent, record, cpu):
-        self.pevent = pevent
-        self.rec = record
+        self._pevent = pevent
+        self._record = record
         self.cpu = cpu
         type = pevent_data_type(pevent, record)
-        self.format = pevent_data_event_from_type(pevent, type)
+        self._format = pevent_data_event_from_type(pevent, type)
 
     def __str__(self):
         return "%d.%d CPU%d %s: pid=%d comm=%s type=%d" % \
@@ -45,33 +45,33 @@ class Event(object):
                 self.num_field("common_pid"), self.comm, self.type)
 
     def __del__(self):
-        free_record(self.rec);
+        free_record(self._record);
 
 
     # TODO: consider caching the results of the properties
     @property
     def comm(self):
-        return pevent_data_comm_from_pid(self.pevent, self.pid)
+        return pevent_data_comm_from_pid(self._pevent, self.pid)
 
     @property
     def name(self):
-        return event_format_name_get(self.format)
+        return event_format_name_get(self._format)
 
     @property
     def pid(self):
-        return pevent_data_pid(self.pevent, self.rec)
+        return pevent_data_pid(self._pevent, self._record)
 
     @property
     def ts(self):
-        return record_ts_get(self.rec)
+        return record_ts_get(self._record)
 
     @property
     def type(self):
-        return pevent_data_type(self.pevent, self.rec)
+        return pevent_data_type(self._pevent, self._record)
 
     def num_field(self, name):
-        f = pevent_find_any_field(self.format, name)
-        val = pevent_read_number_field_py(f, record_data_get(self.rec))
+        f = pevent_find_any_field(self._format, name)
+        val = pevent_read_number_field_py(f, record_data_get(self._record))
         return val
 
 
@@ -86,37 +86,37 @@ class Trace(object):
     used to manage the trace and extract events from it.
     """
     def __init__(self, filename):
-        self.handle = tracecmd_alloc(filename)
+        self._handle = tracecmd_alloc(filename)
 
-        if tracecmd_read_headers(self.handle):
+        if tracecmd_read_headers(self._handle):
             raise FileFormatError("Invalid headers")
 
-        if tracecmd_init_data(self.handle):
+        if tracecmd_init_data(self._handle):
             raise FileFormatError("Failed to init data")
 
-        self.pe = tracecmd_get_pevent(self.handle)
+        self._pevent = tracecmd_get_pevent(self._handle)
 
     @property
     def cpus(self):
-        return tracecmd_cpus(self.handle)
+        return tracecmd_cpus(self._handle)
 
     def read_event(self, cpu):
-        rec = tracecmd_read_data(self.handle, cpu)
+        rec = tracecmd_read_data(self._handle, cpu)
         if rec:
             #rec.acquire()
             #rec.thisown = 1
-            return Event(self.pe, rec, cpu)
+            return Event(self._pevent, rec, cpu)
         return None
 
     def read_event_at(self, offset):
-        res = tracecmd_read_at(self.handle, offset)
+        res = tracecmd_read_at(self._handle, offset)
         # SWIG only returns the CPU if the record is None for some reason
         if isinstance(res, int):
             return None
         rec,cpu = res
         #rec.acquire()
         #rec.thisown = 1
-        ev = Event(self.pe, rec, cpu)
+        ev = Event(self._pevent, rec, cpu)
         return ev
 
     def peek_event(self, cpu):
