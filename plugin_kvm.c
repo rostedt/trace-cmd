eae46f152079cd3603d02258b54504a9aa29f04c
Steven Rostedt <srostedt@redhat.com>
2010-06-17 20:54:06 -0400

    trace-cmd: Add plugin_kvm.so
    Added code to handle the kvm events that happened whil doing a trace
    on a 2.6.34 version of the kernel on my Intel box.
    
    This adds a kvm plugin to process some of the kvm events.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

---
+++ plugin_kvm.c
@@ -0,0 +1,224 @@
+/*
+ * Copyright (C) 2009 Red Hat Inc, Steven Rostedt <srostedt@redhat.com>
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License (not later!)
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "parse-events.h"
+
+#define VMX_EXIT_REASONS			\
+	_ER(EXCEPTION_NMI,	0)		\
+	_ER(EXTERNAL_INTERRUPT,	1)		\
+	_ER(TRIPLE_FAULT,	2)		\
+	_ER(PENDING_INTERRUPT,	7)		\
+	_ER(NMI_WINDOW,		8)		\
+	_ER(TASK_SWITCH,	9)		\
+	_ER(CPUID,		10)		\
+	_ER(HLT,		12)		\
+	_ER(INVLPG,		14)		\
+	_ER(RDPMC,		15)		\
+	_ER(RDTSC,		16)		\
+	_ER(VMCALL,		18)		\
+	_ER(VMCLEAR,		19)		\
+	_ER(VMLAUNCH,		20)		\
+	_ER(VMPTRLD,		21)		\
+	_ER(VMPTRST,		22)		\
+	_ER(VMREAD,		23)		\
+	_ER(VMRESUME,		24)		\
+	_ER(VMWRITE,		25)		\
+	_ER(VMOFF,		26)		\
+	_ER(VMON,		27)		\
+	_ER(CR_ACCESS,		28)		\
+	_ER(DR_ACCESS,		29)		\
+	_ER(IO_INSTRUCTION,	30)		\
+	_ER(MSR_READ,		31)		\
+	_ER(MSR_WRITE,		32)		\
+	_ER(MWAIT_INSTRUCTION,	36)		\
+	_ER(MONITOR_INSTRUCTION,39)		\
+	_ER(PAUSE_INSTRUCTION,	40)		\
+	_ER(MCE_DURING_VMENTRY,	41)		\
+	_ER(TPR_BELOW_THRESHOLD,43)		\
+	_ER(APIC_ACCESS,	44)		\
+	_ER(EPT_VIOLATION,	48)		\
+	_ER(EPT_MISCONFIG,	49)		\
+	_ER(WBINVD,		54)
+
+#define _ER(reason, val)	{ #reason, val },
+struct str_values {
+	const char	*str;
+	int		val;
+};
+
+static struct str_values vmx_exit_reasons[] = {
+	VMX_EXIT_REASONS
+	{ NULL, -1}
+};
+
+static const char *find_vmx_reason(int val)
+{
+	int i;
+
+	for (i = 0; vmx_exit_reasons[i].val >= 0; i++)
+		if (vmx_exit_reasons[i].val == val)
+			break;
+	if (vmx_exit_reasons[i].str)
+		return vmx_exit_reasons[i].str;
+	return "UNKOWN";
+}
+
+static int kvm_exit_handler(struct trace_seq *s, struct record *record,
+			    struct event_format *event, void *context)
+{
+	unsigned long long val;
+
+	if (pevent_get_field_val(s, event, "exit_reason", record, &val, 1) < 0)
+		return -1;
+
+	trace_seq_printf(s, "reason %s", find_vmx_reason(val));
+
+	pevent_print_num_field(s, " rip %0xlx", event, "guest_rip", record, 1);
+
+	return 0;
+}
+
+static int kvm_nested_vmexit_inject_handler(struct trace_seq *s, struct record *record,
+					    struct event_format *event, void *context)
+{
+	unsigned long long val;
+
+	pevent_print_num_field(s, " rip %0x016llx", event, "rip", record, 1);
+
+	if (pevent_get_field_val(s, event, "exit_code", record, &val, 1) < 0)
+		return -1;
+
+	trace_seq_printf(s, "reason %s", find_vmx_reason(val));
+
+	pevent_print_num_field(s, " ext_inf1: %0x016llx", event, "exit_info1", record, 1);
+	pevent_print_num_field(s, " ext_inf2: %0x016llx", event, "exit_info2", record, 1);
+	pevent_print_num_field(s, " ext_int: %0x016llx", event, "exit_int_info", record, 1);
+	pevent_print_num_field(s, " ext_int_err: %0x016llx", event, "exit_int_info_err", record, 1);
+
+	return 0;
+}
+
+static int kvm_nested_vmexit_handler(struct trace_seq *s, struct record *record,
+				     struct event_format *event, void *context)
+{
+	pevent_print_num_field(s, " rip %0x016llx", event, "rip", record, 1);
+
+	return kvm_nested_vmexit_inject_handler(s, record, event, context);
+}
+
+union kvm_mmu_page_role {
+	unsigned word;
+	struct {
+		unsigned glevels:4;
+		unsigned level:4;
+		unsigned quadrant:2;
+		unsigned pad_for_nice_hex_output:6;
+		unsigned direct:1;
+		unsigned access:3;
+		unsigned invalid:1;
+		unsigned cr4_pge:1;
+		unsigned nxe:1;
+	};
+};
+
+static int kvm_mmu_print_role(struct trace_seq *s, struct record *record,
+			      struct event_format *event, void *context)
+{
+	unsigned long long val;
+	static const char *access_str[] =
+		{ "---", "--x", "w--", "w-x", "-u-", "-ux", "wu-", "wux" };
+	union kvm_mmu_page_role role;
+
+	if (pevent_get_field_val(s, event, "role", record, &val, 1) < 0)
+		return -1;
+
+	role.word = (int)val;
+
+	/*
+	 * We can only use the structure if file is of the same
+	 * endianess.
+	 */
+	if (pevent_is_file_bigendian(event->pevent) ==
+	    pevent_is_host_bigendian(event->pevent)) {
+
+		trace_seq_printf(s, "%u/%u q%u%s %s%s %spge %snxe",
+				 role.level,
+				 role.glevels,
+				 role.quadrant,
+				 role.direct ? " direct" : "",
+				 access_str[role.access],
+				 role.invalid ? " invalid" : "",
+				 role.cr4_pge ? "" : "!",
+				 role.nxe ? "" : "!");
+	} else
+		trace_seq_printf(s, "WORD: %08x", role.word);
+
+	pevent_print_num_field(s, " root %u",  event,
+			       "root_count", record, 1);
+
+	if (pevent_get_field_val(s, event, "unsync", record, &val, 1) < 0)
+		return -1;
+
+	trace_seq_printf(s, "%s%c",  val ? "unsync" : "sync", 0);
+
+	return 0;
+}
+static int kvm_mmu_get_page_handler(struct trace_seq *s, struct record *record,
+				    struct event_format *event, void *context)
+{
+	unsigned long long val;
+
+	if (pevent_get_field_val(s, event, "gfn", record, &val, 1) < 0)
+		return -1;
+
+	trace_seq_printf(s, "sp gfn %llx ", val);
+
+	return kvm_mmu_print_role(s, record, event, context);
+}
+
+int PEVENT_PLUGIN_LOADER(struct pevent *pevent)
+{
+	pevent_register_event_handler(pevent, -1, "kvm", "kvm_exit",
+				      kvm_exit_handler, NULL);
+
+	pevent_register_event_handler(pevent, -1, "kvm", "kvm_nested_vmexit",
+				      kvm_nested_vmexit_handler, NULL);
+
+	pevent_register_event_handler(pevent, -1, "kvm", "kvm_nested_vmexit_inject",
+				      kvm_nested_vmexit_inject_handler, NULL);
+
+	pevent_register_event_handler(pevent, -1, "kvmmmu", "kvm_mmu_get_page",
+				      kvm_mmu_get_page_handler, NULL);
+
+	pevent_register_event_handler(pevent, -1, "kvmmmu", "kvm_mmu_sync_page",
+				      kvm_mmu_print_role, NULL);
+
+	pevent_register_event_handler(pevent, -1, "kvmmmu", "kvm_mmu_unsync_page",
+				      kvm_mmu_print_role, NULL);
+
+	pevent_register_event_handler(pevent, -1, "kvmmmu", "kvm_mmu_zap_page",
+				      kvm_mmu_print_role, NULL);
+
+	return 0;
+}
