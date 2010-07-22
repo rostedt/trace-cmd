2e5b970e1dbd1ecb5eb8db292604b98e0c22525a
David Daney <ddaney@caviumnetworks.com>
2010-07-22 14:05:05 -0700

    trace-cmd: Don't SIGSEGV when event field format cannot be parsed (v2).
    print fmt: "page=%p pfn=%lu order=%d migratetype=%d", REC->page, ({ struct page *__pg = (REC->page); int __sec = page_to_section(__pg); (unsigned long)(__pg - __section_mem_map_addr(__nr_to_section(__sec))); }), REC->order, REC->migratetype
    
    This cannot be parsed, leading to a NULL struct event_format* being
    passed to pevent_get_common_field_val, which produces a SIGSEGV.  It
    would be good to get a parsable format from the kernel, but to
    remediate the problem for legacy kernels, we can just return an error
    indicator in this case.  This allows some output from trace-cmd
    report, although perhaps with some missing data.  But this is better
    than crashing.
    
    (v2): Do the check in all pevent_get_*
    
    Signed-off-by: David Daney <ddaney@caviumnetworks.com>
    LKML-Reference: <1279832705-14219-1-git-send-email-ddaney@caviumnetworks.com>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- parse-events.c
+++ parse-events.c
@@ -4464,6 +4464,9 @@ int pevent_get_field_val(struct trace_seq *s, struct event_format *event,
 {
 	struct format_field *field;
 
+	if (!event)
+		return -1;
+
 	field = pevent_find_field(event, name);
 
 	return get_field_val(s, field, name, record, val, err);
@@ -4486,6 +4489,9 @@ int pevent_get_common_field_val(struct trace_seq *s, struct event_format *event,
 {
 	struct format_field *field;
 
+	if (!event)
+		return -1;
+
 	field = pevent_find_common_field(event, name);
 
 	return get_field_val(s, field, name, record, val, err);
@@ -4507,6 +4513,9 @@ int pevent_get_any_field_val(struct trace_seq *s, struct event_format *event,
 			     unsigned long long *val, int err)
 {
 	struct format_field *field;
+
+	if (!event)
+		return -1;
 
 	field = pevent_find_any_field(event, name);
 
