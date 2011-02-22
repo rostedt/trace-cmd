2523a0115966e362175ecc355d3d495fd25edc8d
Steven Rostedt <srostedt@redhat.com>
2011-02-22 18:15:13 -0500

    parse-events: Whitespace fix
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- parse-events.c
+++ parse-events.c
@@ -262,7 +262,7 @@ static int add_new_comm(struct pevent *pevent, char *comm, int pid)
 	cmdlines[pevent->cmdline_count].pid = pid;
 	cmdlines[pevent->cmdline_count].comm = comm;
 	if (cmdlines[pevent->cmdline_count].comm)
-	pevent->cmdline_count++;
+		pevent->cmdline_count++;
 
 	qsort(cmdlines, pevent->cmdline_count, sizeof(*cmdlines), cmdline_cmp);
 	pevent->cmdlines = cmdlines;
