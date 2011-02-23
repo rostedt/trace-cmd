e9fa08f59877a98b65cc2c26129c81778de31279
Steven Rostedt <srostedt@redhat.com>
2011-02-22 19:42:12 -0500

    parse-events: Free token in processing function handler
    A missing free_token() in the function handler of pevent was
    causing a memory leak.
    
    Thanks again to valgrind.
    
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- parse-events.c
+++ parse-events.c
@@ -2371,6 +2371,7 @@ process_func_handler(struct event_format *event, struct pevent_function_handler 
 
 		*next_arg = farg;
 		next_arg = &(farg->next);
+		free_token(token);
 	}
 
 	type = read_token(&token);
