a4490d61861927a2aaad8dd913423a2fcf18bbe4
Rickard Strandqvist <rickard_strandqvist@spectrumdigital.se>
2014-06-24 13:09:10 +0200

    tools lib traceevent: Fix a risk for doing free on uninitialized pointer
    Fix a risk of doing free on an uninitialized pointer.
    
    This was found using a static code analysis program called cppcheck.
    
    Acked-by: Namhyung Kim <namhyung@kernel.org>
    Signed-off-by: Rickard Strandqvist <rickard_strandqvist@spectrumdigital.se>
    Link: http://lkml.kernel.org/r/1403608150-13037-1-git-send-email-rickard_strandqvist@spectrumdigital.se
    Signed-off-by: Jiri Olsa <jolsa@kernel.org>
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- event-parse.c
+++ event-parse.c
@@ -2403,7 +2403,7 @@ process_flags(struct event_format *event, struct print_arg *arg, char **tok)
 {
 	struct print_arg *field;
 	enum event_type type;
-	char *token;
+	char *token = NULL;
 
 	memset(arg, 0, sizeof(*arg));
 	arg->type = PRINT_FLAGS;
@@ -2456,7 +2456,7 @@ process_symbols(struct event_format *event, struct print_arg *arg, char **tok)
 {
 	struct print_arg *field;
 	enum event_type type;
-	char *token;
+	char *token = NULL;
 
 	memset(arg, 0, sizeof(*arg));
 	arg->type = PRINT_SYMBOL;
@@ -2495,7 +2495,7 @@ process_hex(struct event_format *event, struct print_arg *arg, char **tok)
 {
 	struct print_arg *field;
 	enum event_type type;
-	char *token;
+	char *token = NULL;
 
 	memset(arg, 0, sizeof(*arg));
 	arg->type = PRINT_HEX;
