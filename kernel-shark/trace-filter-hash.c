847b690558339c8a2626d02a6462a49f0e20aaec
Steven Rostedt (VMware) <rostedt@goodmis.org>
2018-06-19 18:21:57 -0400

    kernelshark: Change the license of trace-filter-hash.[ch] for LPGL
    trace-filter-hash.h was only licensed as GPL because it include
    trace-hash-local.h, which included an algorithm that was licensed as GPL.
    Now that trace-filter-hash.h has removed that code, it can go to the
    originally intended LGPL license.
    
    By doing this, we can then move the trace-filter-hash.o into libtracecmd
    which is LGPL.
    
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- kernel-shark/trace-filter-hash.c
+++ kernel-shark/trace-filter-hash.c
@@ -1,19 +1,20 @@
 /*
  * Copyright (C) 2009, Steven Rostedt <srostedt@redhat.com>
+ * Copyright (C) 2018 VMware Inc, Steven Rostedt <rostedt@goodmis.org>
  *
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License (not later!)
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation;
+ * version 2.1 of the License (not later!)
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not,  see <http://www.gnu.org/licenses>
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not,  see <http://www.gnu.org/licenses>
  *
  * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  */
