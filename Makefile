rostedt/trace-cmd

commit 1926e78eca0cde64c8fe0a9295394064b1b462e0

trace-cmd: Add C++ files to 'make TAGS'
Add *.cpp and *.hpp to list of files to examine when creating the TAGS
files.

Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

Makefile
@@ -298,7 +298,7 @@ show_gui_make:
 PHONY += show_gui_make
 
 define find_tag_files
-	find . -name '\.pc' -prune -o -name '*\.[ch]' \
+	find . -name '\.pc' -prune -o -name '*\.[ch]' -o -name '*\.[ch]pp' \
 		! -name '\.#' -print
 endef
 

commit e447d66dcd832f4867aecd66ef7464e5f2088f35

trace-cmd: Fix 'make TAGS' that now only adds cpp files
Commit 1926e78eca0c ("trace-cmd: Add C++ files to 'make TAGS'") changed
"make TAGS" to also add cpp files. Unfortunately, it changed the code to
only process cpp files. This fixes that error, so that make TAGS now does
all files for etag processing.

Fixes: 1926e78eca0c ("trace-cmd: Add C++ files to 'make TAGS'")
Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

Makefile
@@ -298,7 +298,7 @@ show_gui_make:
 PHONY += show_gui_make
 
 define find_tag_files
-	find . -name '\.pc' -prune -o -name '*\.[ch]' -o -name '*\.[ch]pp' \
+	find . -name '\.pc' -prune -o -name '*\.[ch]' -print -o -name '*\.[ch]pp' \
 		! -name '\.#' -print
 endef
 