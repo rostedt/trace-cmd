rostedt/trace-cmd

commit c7d73430e505b3344113a6109ea01822c0bd9522

trace-cmd: Make the build to tell when python-dev is missing
Currently, the build system will complain when swig is missing, since that
will prevent it to build the python C modules, but in case swig is present
but python-dev is not installed, the build will just silently skip them.
This patch just makes the build to report a message when that happens.

Signed-off-by: Vladislav Valtchev (VMware) <vladislav.valtchev@gmail.com>
Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

Makefile
@@ -121,6 +121,8 @@ ifeq ($(shell sh -c "pkg-config --cflags $(PYTHON_VERS) > /dev/null 2>&1 && echo
 	PYTHON_PLUGINS := plugin_python.so
 	BUILD_PYTHON := $(PYTHON) $(PYTHON_PLUGINS)
 	BUILD_PYTHON_WORKS := 1
+else
+	BUILD_PYTHON := report_nopythondev
 endif
 endif # NO_PYTHON
 
@@ -371,6 +373,11 @@ clean:
 report_noswig: force
 	$(Q)echo
 	$(Q)echo "    NO_PYTHON forced: swig not installed, not compiling python plugins"
+	$(Q)echo
+
+report_nopythondev: force
+	$(Q)echo
+	$(Q)echo "    python-dev is not installed, not compiling python plugins"
 	$(Q)echo
 
 PYTHON_INCLUDES = `pkg-config --cflags $(PYTHON_VERS)`

commit a66fd8bfca62cb7359ae7098250ce9735c825413

trace-cmd: Set NO_PYTHON if python-dev package not found
The build fails with:

make[1]: `/work/git/trace-cmd.git/lib/trace-cmd/libtracecmd.a' is up to date.

    python-dev is not installed, not compiling python plugins

  COMPILE PLUGIN OBJ     plugin_python.o
Package python was not found in the pkg-config search path.
Perhaps you should add the directory containing `python.pc'
to the PKG_CONFIG_PATH environment variable
No package 'python' found
plugin_python.c:1:20: error: Python.h: No such file or directory
plugin_python.c: In function ‘load_plugin’:

Because even thought the python-dev package was found to be missing
the NO_PYTHON was not set, and that is used to determine building
the python plugin.

Fixes: c7d73430e505 ("trace-cmd: Make the build to tell when python-dev is missing")
Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

Makefile
@@ -123,6 +123,7 @@ ifeq ($(shell sh -c "pkg-config --cflags $(PYTHON_VERS) > /dev/null 2>&1 && echo
 	BUILD_PYTHON_WORKS := 1
 else
 	BUILD_PYTHON := report_nopythondev
+	NO_PYTHON = 1
 endif
 endif # NO_PYTHON
 