5c5ab86dde2c8eebd84051056b6f9417de6c09e7
Steven Rostedt (VMware) <rostedt@goodmis.org>
2020-01-22 12:06:03 -0500

    trace-cmd: Remove unused trace_util function declarations
    Due to conflict resolutions, pulling in a patch to add VM tracing protocol
    messages also accidentally pulled in old declarations that were previously
    deleted. This caused ctracecmd.so python helper to reference them and fail
    to load python modules because of their absence.
    
    Reported-by: Johannes Berg <johannes@sipsolutions.net>
    Fixes: 8790da96c26d ("trace-cmd: Add VM tracing protocol messages")
    Signed-off-by: Steven Rostedt (VMware) <rostedt@goodmis.org>

--- include/trace-cmd/trace-cmd.h
+++ include/trace-cmd/trace-cmd.h
@@ -398,17 +398,6 @@ int tracecmd_msg_recv_trace_resp(struct tracecmd_msg_handle *msg_handle,
 /* --- Plugin handling --- */
 extern struct tep_plugin_option trace_ftrace_options[];
 
-int trace_util_add_options(const char *name, struct tep_plugin_option *options);
-void trace_util_remove_options(struct tep_plugin_option *options);
-int trace_util_add_option(const char *name, const char *val);
-int trace_util_load_plugins(struct tep_handle *pevent, const char *suffix,
-			    int (*load_plugin)(struct tep_handle *pevent,
-					       const char *path,
-					       const char *name,
-					       void *data),
-			    void *data);
-struct tep_plugin_option *trace_util_read_plugin_options(void);
-void trace_util_free_options(struct tep_plugin_option *options);
 char **trace_util_find_plugin_files(const char *suffix);
 void trace_util_free_plugin_files(char **files);
 
