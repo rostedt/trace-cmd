2668b130d343585d3fd825a63333070697f55930
Steven Rostedt (Google) <rostedt@goodmis.org>
2023-06-02 04:09:55 -0400

    trace-cmd agent: Add "IP" to -N argument in help message
    The trace-cmd agent -h message does not specify that an IP needs to be
    given as an argument. Update the help message to reflect that.
    
    Link: https://lore.kernel.org/linux-trace-devel/20230602040955.6195a184@rorschach.local.home
    
    Bugzilla: https://bugzilla.kernel.org/show_bug.cgi?id=216291
    Reported-by: Alexander Aring <aahringo@redhat.com>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- tracecmd/trace-usage.c
+++ tracecmd/trace-usage.c
@@ -334,7 +334,7 @@ static struct usage_help usage_help[] = {
 	{
 		"agent",
 		"listen on a vsocket for trace clients",
-		" %s agent -p port[-D][-N][-P cid]\n"
+		" %s agent -p port[-D][-N IP][-P cid]\n"
 		"          Creates a vsocket to listen for clients.\n"
 		"          -N Connect to IP via TCP instead of vsockets\n"
 		"             *** Insecure setting, only use on a trusted network ***\n"
